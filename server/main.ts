4f9a45e2e2e60c45121c932d5865a7cef446039f
b <no@email.address>
2021-06-20 23:23:08 +0200

    Add HTTPS when available

--- server/main.ts
+++ server/main.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import path from 'path';
 import * as http from 'http';
+import * as https from 'https';
+import * as fs from 'fs';
 
 import { g } from '../shared/globals';
 
@@ -9,6 +11,8 @@ import * as sockets from './sockets';
 import * as db from './db';
 import * as runtime_settings from './runtime_settings';
 import { logger, logger_http, info } from './log';
+
+const keypath = (filename: string) => `/etc/letsencrypt/live/plaza.tooie.net/${filename}`;
 
 async function main()
 {
@@ -25,7 +29,24 @@ async function main()
     app.use(express.static(path.join(__dirname, '../dist/plaza/')));
     api.register_all(app);
 
-    const server = http.createServer(app);
+    var server: any = undefined;
+
+    if (fs.existsSync(keypath('privkey.pem')))
+    {
+        logger.info("Key exists, hosting HTTPS");
+        server = https.createServer({
+                key: fs.readFileSync(keypath('privkey.pem')),
+                cert: fs.readFileSync(keypath('cert.pem')),
+                ca: fs.readFileSync(keypath('chain.pem')),
+            },
+            app);
+    }
+    else
+    {
+        logger.info("Key does not exist, hosting HTTP");
+        server = http.createServer(app);
+    }
+
     server.listen(g.server.port)
 
     sockets.init(server);
