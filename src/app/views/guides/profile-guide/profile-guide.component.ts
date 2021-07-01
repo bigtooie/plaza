cacca9819c13198295e8cb6c3f1f877e3b18ee08
b <no@email.address>
2021-07-01 17:50:12 +0200

    Add "my profile" guide

--- src/app/views/guides/profile-guide/profile-guide.component.ts
+++ src/app/views/guides/profile-guide/profile-guide.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from '@services/user.service';
+import { g } from '@shared/globals';
+
 @Component({
   selector: 'app-profile-guide',
   templateUrl: './profile-guide.component.html',
   styleUrls: ['./profile-guide.component.scss']
 })
-export class ProfileGuideComponent implements OnInit {
+export class ProfileGuideComponent implements OnInit
+{
+    g=g;
 
-  constructor() { }
+  constructor(public userService: UserService) { }
 
   ngOnInit(): void {
   }
